be392a2cbfd808b15997d00f4548ebb71dea0be8
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-08-05 22:32:24 +0530

    Parenthesis Balanced or not

---
+++ Cycle Sheet/Cycle Sheet 1/Q6.cpp
@@ -0,0 +1,46 @@
+/*
+  Q6. Write a program to check Balanced Parentheses for an expression using Stacks.
+For example ‘{([])}’ is balanced but ‘{([})]‘ is not. Get the expression in string format.
+*/
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+// Function to check whether two characters are opening 
+// and closing of same type. 
+bool ArePair(char opening,char closing)
+{
+	if(opening == '(' && closing == ')') return true;
+	else if(opening == '{' && closing == '}') return true;
+	else if(opening == '[' && closing == ']') return true;
+	return false;
+}
+bool AreParanthesesBalanced(string exp)
+{
+	stack<char>  S;
+	for(int i =0;i<exp.length();i++)
+	{
+		if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+			S.push(exp[i]);
+		else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+		{
+			if(S.empty() || !ArePair(S.top(),exp[i]))
+				return false;
+			else
+				S.pop();
+		}
+	}
+	return S.empty() ? true:false;
+}
+
+int main()
+{
+	/*Code to test the function AreParanthesesBalanced*/
+	string expression;
+	cout<<"Enter an expression:  "; // input expression from STDIN/Console
+	cin>>expression;
+	if(AreParanthesesBalanced(expression))
+		cout<<"Balanced\n";
+	else
+		cout<<"Not Balanced\n";
+}
